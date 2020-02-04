8ec0cf982b968f326277eade220ab1bc01d903db
Daniel Reina <dannyflatiron@gmail.com>
2020-02-04 02:31:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,6 +8,13 @@ class InteractiveRecord
   end
 
   def self.column_names
+    sql = "pragma table_info('#{table_name}')"
 
+      table_info = DB[:conn].execute(sql)
+      column_names = []
+      table_info.each do |row|
+        column_names << row["name"]
+      end
+    column_names.compact
   end
 end
